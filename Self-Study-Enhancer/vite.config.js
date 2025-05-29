0404395801b6cfcce21c23d1cc684ee6aa035736
shakti-shakti <kumarshakti847402@gmail.com>
2025-05-29 23:05:33 +0530

    Update vite.config.js

--- Self-Study-Enhancer/vite.config.js
+++ Self-Study-Enhancer/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/my-pwa-app1/',  // <-- your repo name with trailing slash
-  plugins: [react()],
+  base: '/my-pwa-app1/', // <--- This MUST match your GitHub repo name exactly
+  plugins: [react(), VitePWA()]
 })
